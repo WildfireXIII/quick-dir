8524550d73d3dd101c1aef8b2b4f7a9388f4b42e
Nathan Martindale (WildfireXIII) <nmblenderdude0@gmail.com>
2016-02-15 23:09:35 -0600

    Continued working on changing KWA

--- KeyWordAnalyzer.h
+++ KeyWordAnalyzer.h
@@ -13,32 +13,37 @@
 
 using namespace std;
 
-class KeyWord
+namespace dwl
 {
-	public:
-		string FullText;
-		vector<int>* Keys;
-};
 
-class KeyWordAnalyzer
-{
-	private:
-		// vector<vector<KeyWord> >* m_groups; // DON'T NEED, just single master group to start out with (prob don't even need it here)
-		vector<KeyWord>* m_completed;
+	class KeyWord
+	{
+		public:
+			string FullText;
+			vector<int>* Keys;
+	};
 
-		string m_userKeys; // users typed KEYS ONLY
-		string m_userWord; // users typed word (auto fills from keys)
+	class KeyWordAnalyzer
+	{
+		private:
+			// vector<vector<KeyWord> >* m_groups; // DON'T NEED, just single master group to start out with (prob don't even need it here)
+			vector<KeyWord>* m_completed;
+
+			//string m_userKeys; // users typed KEYS ONLY
+			string m_userWord; // users typed word (auto fills from keys)
+			
+			void analyzeGroup(vector<KeyWord> group, int letter);
 		
-		void analyzeGroup(vector<KeyWord> group, int letter);
-	
-	public:
-		KeyWordAnalyzer();
-		KeyWordAnalyzer(vector<string>* wordList);
-		~KeyWordAnalyzer();
+		public:
+			KeyWordAnalyzer();
+			KeyWordAnalyzer(vector<string>* wordList);
+			~KeyWordAnalyzer();
 
-		vector<KeyWord>* getKeyWords();
-		
-		vector<KeyWord>* analyzeKeyWords(vector<string>* wordList);
+			vector<KeyWord>* getKeyWords();
+			string getFilledWord();
+			
+			vector<KeyWord>* analyzeKeyWords(vector<string>* wordList);
 
-		vector<KeyWord>* filterKeys
-};
+			vector<KeyWord>* filterKeys(string keys); // returns list of words that only match keys so far
+	};
+}
