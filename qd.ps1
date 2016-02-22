c34095698201b980bdac3eb78d186029388a746f
Nathan Martindale (WildfireXIII) <nmblenderdude0@gmail.com>
2016-02-21 19:32:56 -0600

    Fixed powershell script to work once installed

--- qd.ps1
+++ qd.ps1
@@ -1,4 +1,7 @@
-qdapplication.exe # eventually put subdir in front
+$BIN_DIR = $env:BIN_DIR
+$applicationFolder = "$BIN_DIR/QuickDir"
+
+"$applicationFolder/qdapplication.exe" 
 $newPath = get-content "C:\dwl\tmp\_FILELAYER_CWD.dat"
 del "C:\dwl\tmp\_FILELAYER_CWD.dat"
 cd $newPath
