e694614793dcf608998973046224565ba6d75c4f
Nathan Martindale (WildfireXIII) <nmblenderdude0@gmail.com>
2016-05-09 22:14:21 -0500

    Added things

--- qd.ps1
+++ qd.ps1
@@ -1,7 +1,7 @@
 $BIN_DIR = $env:BIN_DIR
 $applicationFolder = "$BIN_DIR/QuickDir"
 
-"$applicationFolder/qdapplication.exe" 
+. "$applicationFolder/qdapplication.exe"
 $newPath = get-content "C:\dwl\tmp\_FILELAYER_CWD.dat"
 del "C:\dwl\tmp\_FILELAYER_CWD.dat"
 cd $newPath
